28fed72da054ef742452c2633799b1708639a7b2
김현희 <tta_bi@naver.com>
2021-12-27 12:15:57 +0900

    상대 경로를 이용한 모듈 내 함수 import

---
+++ datas/test/test_module.py
@@ -0,0 +1,7 @@
+
+
+def test01(self):
+    print('1번 테스트 함수입니다.')
+
+def test02(self):
+    print('2번 테스트 함수입니다.')
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datas.dog import Dog
 from datas.cow import Cow
 from datas.horse import Horse
 from datas.test.test import Mytest
+from datas.test.test_module import test01, test02
 
 
 dog1 = Dog('해피', 2016, '푸들')  # 변수를 만들어줘야 객체를 불러올 수 있음.
@@ -66,3 +67,8 @@ horse1.run()
 
 cat1.play_with_human()
 cat1.run()
+
+
+
+test01()
+test02()
